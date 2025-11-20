05869bc9a250384534ab234ea76b9adc5cb329c2
Abhirami-S-S <abhiramisanal677@gmail.com>
2025-11-20 22:10:41 +0530

    Removed old .py files

--- Semester_1/rec_list.py
+++
@@ -1,9 +0,0 @@
-def list_join(l,index=0):
-    if(index>=(len(l))):
-        return ""
-    else:
-        if index==(len(l)-1):
-            return str(l[index])
-        return str(l[index])+"-"+list_join(l,index+1)
-l1=["a","b","c"]
-print(list_join(l1))
