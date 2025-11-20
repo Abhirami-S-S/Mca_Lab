7778b8fd508deddd5a569039726b36ea097752ab
Abhirami-S-S <abhiramisanal677@gmail.com>
2025-11-20 22:07:19 +0530

    Removed old .py files

--- is.py
+++
@@ -1,8 +0,0 @@
-a=input("Enter :").split(',')
-result=[]
-for c in a:
-    if " " in c:
-        result.append(True)
-    else:
-        result.append(False)
-print(result)
--- string.py
+++
@@ -1,6 +0,0 @@
-s=input("Enter a string (minimum length 7 and odd length ):")
-if(len(s)<7 and len(s)%2==0):
-    print("Invalid Input")
-else:
-    n=len(s)//2
-    print(s[n-1:n+2])
