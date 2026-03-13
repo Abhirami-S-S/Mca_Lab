70e8b4e87df809d21fa3299ce01e47f94878a769
Abhirami S S <abhiramisanal677@gmail.com>
2026-03-13 10:27:37 +0530

    Add files via upload

---
+++ Semester2/Java/Pg21.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
+
+public class Pg21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1, n, sum = 0, m;
+
+        System.out.print("Enter the count: ");
+        n = sc.nextInt();
+
+        while (i <= n) {
+            System.out.print("Enter number " + i + " : ");
+
+            try {
+                m = sc.nextInt();
+
+                if (m < 0) {
+                    throw new NegativeNumberException("Value must be positive");
+                }
+
+                sum = sum + m;
+                i++;
+            }
+            catch (NegativeNumberException e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
+
+        System.out.println("Average = " + sum/n);
+    }
+}
